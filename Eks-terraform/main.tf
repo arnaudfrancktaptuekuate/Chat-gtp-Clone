7e2d9d1a62f7ffe62b80fa76019831c5dd4a03aa
arnaudfrancktaptuekuate <131401143+arnaudfrancktaptuekuate@users.noreply.github.com>
2024-04-30 00:20:06 -0500

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -79,7 +79,7 @@ resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryRea
 }
 
 #create node group
-resource "aws_eks_node_group" "example" {
+resource "aws_eks_node_group" "example_node_group" {
   cluster_name    = aws_eks_cluster.example.name
   node_group_name = "Node-cloud"
   node_role_arn   = aws_iam_role.example1.arn
